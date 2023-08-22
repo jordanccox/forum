0846dd9ac1424a5f952ed98d2e249257f56e2f69
Jordan Cox <jordancox747@outlook.com>
2023-08-22 07:58:21 -0600

    initial

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// your code here
+const button = document.querySelector('#submit');
+
+button.addEventListener('click', () => {
+  const user = document.querySelector('#name').value;
+  const msg = document.querySelector('#message').value;
+
+  const p = document.createElement('p');
+  const pContent = document.createTextNode(msg);
+
+  p.appendChild(pContent);
+
+  const span = document.createElement('span');
+  const postedBy = document.createTextNode("Posted By: ");
+
+  span.appendChild(postedBy);
+
+  const bold = document.createElement('b');
+  const userName = document.createTextNode(user);
+
+  bold.appendChild(userName);
+
+  document.querySelectorAll('div')[0].append(p);
+  document.querySelectorAll('div')[0].append(span);
+  document.querySelectorAll('div')[0].append(bold);
+});
